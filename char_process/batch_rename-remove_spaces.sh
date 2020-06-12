3c92c7b46205583ca96f35f485c523998255b10b
wsl <1323579954@qq.com>
2020-06-12 15:34:44 +0800

    add char_process/batch_rename-remove_spaces.sh file

---
+++ char_process/batch_rename-remove_spaces.sh
@@ -0,0 +1,8 @@
+for i in * ; do
+		[ -e "$i" ] || continue
+		no_space_i=`echo "$i" | sed s/[[:space:]]//g`
+		echo "$no_space_i"
+		if [[ "$i" != "$no_space_i" ]]; then 
+			mv -- "$i" "$no_space_i"  # -- 可以防止文件名里面有- 造成参数解析错误
+		fi
+done
