82a1a09be70deb9e0fe3bc3207938676f87ddef3
wsl <1323579954@qq.com>
2020-06-22 08:05:20 +0800

    add check env function for tools.sh

--- tools.sh
+++ tools.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+setEnv(){
+		if [ ! -d ~/.bin ] ; then 
+			PATH=$PATH:~/.bin
+		fi
+}
 for i in */*.sh ; do 
 		[ -e $i ] || continue 
+		setEnv
 		echo $i
 		file=$(echo $i | sed s/.sh//g | sed s#.*/##g) 
 		echo $file
 		cp  -- $i ~/.bin/$file
 done
+
