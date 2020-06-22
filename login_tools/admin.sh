45ef09c983d9807fc5e5df0616387ecacfbd2a94
wsl <1323579954@qq.com>
2020-06-22 19:11:16 +0800

    rewrite login_tools/admin/sh

--- login_tools/admin.sh
+++ login_tools/admin.sh
@@ -1,14 +1,63 @@
-if [[ x"$1" == x"" ]]; then 
-		echo "请输入server参数" 
-elif [[ x"$1" != x"s""[0-9]*" ]] ;then 
-	echo "Server不存在,Server以s开头数字结尾。"
-fi
+#!/bin/bash
 s=(root@zqa.me root@tc.askaskask.cn)
+getList(){
+		echo "你拥有下列服务器"
+		for i in ${!s[@]} ; do 
+				echo ${s[$i]};
+		done
+}
+getHelp(){
+		echo "第一个参数为command，它可以是list help add mysql ssh等"
+		echo "第二个参数为服务器的地址，以s开头 数字结尾"
+}
 
-for i in ${!s[@]} ; do
+addServer(){
+	echo "正在完善中"
+}
+
+sshAdmin(){
+	for i in ${!s[@]} ; do
 	if [[ x"$1" == x"s"$i ]]; then 
 			ssh ${s[$i]} ; 
+	fi 
+	done
+}
+mysqlAdmin(){
+	echo "正在完善中"
+}
+# main
+# 帮助相关
+if [[ x"$1" == x"" || x"$1" == x"help" ]]; then 
+	getHelp
+elif [[ x"$1" == x"list" ]];then
+	getList
+fi
+#addServer相关
+if [[ x"$1" == x"add" ]];then 
+	addServer
+fi
+
+# ssh相关
+if  [[ x"$1" == x"ssh" ]];then
+	if [[ x"$2" == x"" ]];then
+		echo "ssh Server未选择，请查看帮助手册"
+		exit -1
 	fi
-done
+	if [[ x"$2" != xs[0-9]* ]] ;then
+		echo "Server输入不正确,Server必须以s开头 以数字结尾"
+		exit -1
+	fi
+	sshAdmin "$2"
+
+fi
+
+# mysql相关
+if [[ x"$1" == x"mysql" ]];then 
+	mysqlAdmin
+fi
 
 
+
+
+
+		
