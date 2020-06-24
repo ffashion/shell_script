0b509beb55d844098df423f95e590f892e517969
wsl <1323579954@qq.com>
2020-06-24 20:16:59 +0800

    login_tools/admin.sh add addServer and delServer function

--- login_tools/admin.sh
+++ login_tools/admin.sh
@@ -1,63 +1,82 @@
 #!/bin/bash
-s=(root@zqa.me root@tc.askaskask.cn)
+s=(root@zqa.me root@tc.askaskask.cn )
 getList(){
-		echo "你拥有下列服务器"
-		for i in ${!s[@]} ; do 
-				echo ${s[$i]};
-		done
+                echo "你拥有下列服务器"
+                for i in ${!s[@]} ; do
+                                echo ${s[$i]};
+                done
 }
 getHelp(){
-		echo "第一个参数为command，它可以是list help add mysql ssh等"
-		echo "第二个参数为服务器的地址，以s开头 数字结尾"
+                echo "第一个参数为command，它可以是list help add del mysql ssh等"
+                echo "第二个参数为服务器的地址，以s开头 数字结尾"
 }
 
-addServer(){
-	echo "正在完善中"
+addSSHServer(){
+        # &表示引用前面的pattern
+        sed  -i "s/^s=(\(.*[[:space:]]\)\{0,\}/&`echo -n $1` /g" `echo $0`
 }
-
+delSSHServer(){
+        # \1 \2 \3表示反向引用
+        sed -i "s/\(s=(\)\(.*\)"$1" \(.*\)/\1\2\3/g" `echo $0`
+}
+addMYSQLServer(){
+        echo "正在完善中"
+}
+delMYSQLServer(){
+        echo "正在完善中"
+}
 sshAdmin(){
-	for i in ${!s[@]} ; do
-	if [[ x"$1" == x"s"$i ]]; then 
-			ssh ${s[$i]} ; 
-	fi 
-	done
+        for i in ${!s[@]} ; do
+        if [[ x"$1" == x"s"$i ]]; then
+                        ssh ${s[$i]} ;
+        fi
+        done
 }
 mysqlAdmin(){
-	echo "正在完善中"
+        echo "正在完善中"
 }
 # main
 # 帮助相关
-if [[ x"$1" == x"" || x"$1" == x"help" ]]; then 
-	getHelp
+if [[ x"$1" == x"" || x"$1" == x"help" ]]; then
+        getHelp
 elif [[ x"$1" == x"list" ]];then
-	getList
+        getList
 fi
 #addServer相关
-if [[ x"$1" == x"add" ]];then 
-	addServer
+if [[ x"$1" == x"add" ]];then
+        if [[ x"$2" == x"ssh" ]];then
+                        delSSHServer "$3"
+        elif [[ x"$2" == x"mysql" ]];then
+                        delMYSQLServer
+        else
+                        echo "请在del后面选择ssh或者mysql"
+        fi
 fi
-
+#delServer相关
+if [[ x"$1" == x"del" ]];then
+        if [[ x"$2" == x"ssh" ]];then
+                        delSSHServer "$3"
+        elif [[ x"$2" == x"mysql" ]];then
+                        delMYSQLServer
+        else
+                        echo "请在add后面选择ssh或者mysql"
+        fi
+fi
 # ssh相关
 if  [[ x"$1" == x"ssh" ]];then
-	if [[ x"$2" == x"" ]];then
-		echo "ssh Server未选择，请查看帮助手册"
-		exit -1
-	fi
-	if [[ x"$2" != xs[0-9]* ]] ;then
-		echo "Server输入不正确,Server必须以s开头 以数字结尾"
-		exit -1
-	fi
-	sshAdmin "$2"
+        if [[ x"$2" == x"" ]];then
+                echo "ssh Server未选择，请查看帮助手册"
+                exit -1
+        fi
+        if [[ x"$2" != xs[0-9]* ]] ;then
+                echo "Server输入不正确,Server必须以s开头 以数字结尾"
+                exit -1
+        fi
+        sshAdmin "$2"
 
 fi
 
 # mysql相关
-if [[ x"$1" == x"mysql" ]];then 
-	mysqlAdmin
+if [[ x"$1" == x"mysql" ]];then
+        mysqlAdmin
 fi
-
-
-
-
-
-		
