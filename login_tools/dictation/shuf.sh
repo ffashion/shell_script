42cfaf3e586fb7cd7a783ada7a42c64de5fcf91d
ffashion <1323579954@qq.com>
2021-09-30 22:52:11 +0800

    remove login_tools/dictation

--- login_tools/dictation/shuf.sh
+++
@@ -1,4 +0,0 @@
-for i in {300,5700,300};do
-		shuf  ${i}.txt > ${i}.txt.tmp
-		mv ${i}.txt.tmp ${i}.txt
-done
--- login_tools/dictation/sort.sh
+++
@@ -1,4 +0,0 @@
-sort 5500_all.txt > sort/5500_all_sort.txt
-for i in {300..5700..300};do
-		sort ${i}.txt > sort/${i}_sort.txt
-done
